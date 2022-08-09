ee7e900b9461823759f0f2f4fc283f22aec7870c
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-09 12:31:48 +0900

    JSXで構造を作成、 CSSでスタイリング

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,44 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <div></div>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>あああああ</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              <p>いいいいい</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <div></div>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>うううう</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
