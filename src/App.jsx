2a43583ee680a8ec44ce640275d840d8475602f3
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-09 13:05:27 +0900

    タスクの追加機能

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,40 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  //入力するTODOの値
+  const [todoText, setTodoText] = useState("");
+  //未完了のTODOリスト
   const [incompleteTodos, setIncompleteTodos] = useState([
     "ああああ",
     "いいいい"
   ]);
+  //完了のTODOリスト
   const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+
+  //TODOが入力された時にステートの値を変更
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  //追加ボタンを押した時に入力されたTODOの値を未完了のTODOリストに追加
+  const onClickAdd = () => {
+    //何も入力せずに追加ボタンを押した場合処理を中断
+    if (todoText === "") return;
+
+    //分割代入を利用して未完了のTODOリストに入力されたTODOを追加
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    //入力ボックスの値を空に
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
